9f7ecfc4846c39fb714a7f8e71c5a2893d5cd28c
Dakota Clark <dakotaclark92@yahoo.com>
2020-11-22 04:22:38 +0000

    validates_uniqueness

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -12,7 +12,22 @@ class RegistrationController < ApplicationController
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+   # if params[:name] == "" || params[:email] == "" || params[:password] == ""
+   #   flash[:notice] = "<h3 class ='alert'>Please fill in info!</h3>"
+   #   redirect_to '/registrations'
+   #   
+   # else
+   #   if @user
+   #     flash[:notice] = "<h3>User already exists.</h3>"
+   #     redirect_to '/registrations'
+   #   else
+   #     @user = User.create!(:name => params[:name], :email => params[:email], :password => params[:password])
+   #     session[:user_id] = @user.id
+   #     redirect '/users/home'
+   #   end
+   # end
+    
+    @user = User.create!(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
 
